1821a61444ecf1f5c6c26f444b8f7adb96741070
Ricardo Pérez <ricpelo@gmail.com>
2016-12-05 20:30:22 +0100

    Revert "Alquiler con listas desplegables"
    This reverts commit e1dd38175b9d880924be97dcc82cd1e26159f31f.

--- controllers/AlquileresController.php
+++ controllers/AlquileresController.php
@@ -8,7 +8,6 @@ use app\models\Alquiler;
 use app\models\AlquilerSearch;
 use app\models\Socio;
 use app\models\Pelicula;
-use yii\db\Expression;
 use yii\helpers\Url;
 
 class AlquileresController extends \yii\web\Controller
@@ -21,25 +20,13 @@ class AlquileresController extends \yii\web\Controller
             if ($model->validate()) {
                 $alquiler = new Alquiler;
                 if ($alquiler->alquilar($model->numero, $model->codigo)) {
-                    return $this->redirect(Url::to(['alquileres/index']));
+                    return $this->redirect(Url::to(['alquileres/alquilar']));
                 }
             }
         }
 
-        $socios = Socio::find()
-            ->select(new Expression("numero || ' ' || nombre as nombre, numero"))
-            ->indexBy('numero')
-            ->column();
-
-        $peliculas = Pelicula::find()
-            ->select(new Expression("codigo || ' ' || titulo as titulo, codigo"))
-            ->indexBy('codigo')
-            ->column();
-
         return $this->render('alquilar', [
             'model' => $model,
-            'socios' => $socios,
-            'peliculas' => $peliculas,
         ]);
     }
 
--- views/alquileres/alquilar.php
+++ views/alquileres/alquilar.php
@@ -5,18 +5,12 @@ use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\AlquilerForm */
-/* @var $socios array */
-/* @var $peliculas array */
 /* @var $form ActiveForm */
 ?>
 <div class="alquileres-alquilar">
     <?php $form = ActiveForm::begin(); ?>
-        <?= $form->field($model, 'numero')->dropDownList($socios, [
-                'prompt' => 'Seleccione un socio...',
-            ]) ?>
-        <?= $form->field($model, 'codigo')->dropDownList($peliculas, [
-                'prompt' => 'Seleccione una película...',
-            ]) ?>
+        <?= $form->field($model, 'numero') ?>
+        <?= $form->field($model, 'codigo') ?>
         <div class="form-group">
             <?= Html::submitButton('Alquilar', ['class' => 'btn btn-primary']) ?>
         </div>
